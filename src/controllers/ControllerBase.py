e747f911b7261217f7f64e29020f1a7b6ff2a370
Danny <danny@lilytronics.nl>
2024-11-06 21:07:07 +0100

    add base controller

---
+++ src/controllers/ControllerBase.py
@@ -0,0 +1,16 @@
+"""
+Base controller for all tools.
+Make a subclass for each tool from this base controller.
+"""
+
+
+class ControllerBase:
+    pass
+
+
+if __name__ == "__main__":
+
+    class TestController(ControllerBase):
+        pass
+
+    c = TestController()
